04378a7f9e97adcfea4d06915dbb037632b5a966
Jlbejarano662 <jasmin.1207@hotmail.com>
2022-11-03 12:03:41 -0500

    juego terminado

---
+++ js/acierto-error.js
@@ -0,0 +1,22 @@
+//completa el codigo 
+function acierto(lasTarjetas) {
+  lasTarjetas.forEach(function(elemento) {
+    //pista aca falta llamar la función
+    elemento.classList.add("acertada");
+  });
+}
+  //pista aca falta crear  la función :(
+ function error(lasTarjetas) {
+  lasTarjetas.forEach(function(elemento) {
+    elemento.classList.add("error");
+  });
+
+  document.querySelector("#sonido-error").play();
+  //averigua para que sirve el metodo remove
+  setTimeout(function() {
+    lasTarjetas.forEach(function(elemento) {
+      elemento.classList.remove("descubierta");
+      elemento.classList.remove("error");
+    });
+  }, 1000);
+}
